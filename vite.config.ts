6e68f5d7a6bea19b772e699e9b6e1ccdf16fc4ca
Icaro Gago <icaroggo@gmail.com>
2025-05-28 17:18:13 -0300

    atualizando plugin react

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import reactSWC from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -8,7 +8,7 @@ export default defineConfig({
     host: "::",
     port: 8080,
   },
-  plugins: [react()],
+  plugins: [reactSWC()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
